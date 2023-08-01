e1ab2fed72469eb355ec857e212588682088b571
burcu apaydin <ndabandaba31@gmail.com>
2023-08-02 01:13:33 +0200

    Added Hero component

---
+++ app/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+const Hero = () => {
+    return (
+        <div
+            className="flex items-end w-full h-screen bg-gradient-to-tr from-red-900 via-red-600 to-red-400"
+        >
+            <h1 className="text-4xl sm:text-4xl md:text-6xl lg:text-7xl px-4 py-2">
+                Unleash your desires with Tycoon’s Pact.
+            </h1>
+        </div>
+    );
+};
+
+export default Hero;
