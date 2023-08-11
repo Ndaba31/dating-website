02f8e51df2b971db883bf77b2e8f8621d3757796
burcu apaydin <ndabandaba31@gmail.com>
2023-08-11 05:19:59 +0200

    Dynamic routing done. PumpkinInfo edited Sexy work

--- app/matches/page.tsx
+++ app/matches/page.tsx
@@ -1,7 +1,32 @@
+import Footer from "../components/Footer"
+import PumpkinInfo from "../components/PumpkinInfo"
+import { peopleData } from "../page"
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+    title: 'Pumpkins',
+}
 
 const Matches = () => {
     return (
-        <div>Hello My Pumpkins</div>
+        <main>
+            {
+                peopleData.map(({ name, bio, imgSrc, hickies, pumpkins, instagram, profile }, index) => (
+                    <PumpkinInfo
+                        key={index}
+                        page='matches'
+                        name={name}
+                        bio={bio}
+                        imgSrc={imgSrc}
+                        hickies={hickies}
+                        pumpkins={pumpkins}
+                        instagram={instagram}
+                        profile={profile}
+                    />
+                ))
+            }
+            <Footer />
+        </main>
     )
 }
 
