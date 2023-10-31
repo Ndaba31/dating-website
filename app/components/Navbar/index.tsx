6abb2b3dc02fd616fa7659c93712735fe8e59adb
burcu apaydin <ndabandaba31@gmail.com>
2023-10-31 23:47:54 +0200

    Added context api

--- app/components/Navbar/index.tsx
+++ app/components/Navbar/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Link from 'next/link'
@@ -14,8 +15,8 @@ const Navbar = () => {
                     <Link href='/' className='text-3xl'>Pumpkin</Link>
                 </div>
                 <div className="flex items-center space-x-4">
-                    <button className='text-white'>
-                        <FontAwesomeIcon icon={faCircleUser} size='sm' className='text-white' />
+                    <button onClick={() => { console.log('Ndaba'); }} className='md:hidden'>
+                        <FontAwesomeIcon icon={faCircleUser} size='xl' />
                     </button>
                     <Link href='login' className='hidden md:block px-4 py-2 text-lg hover:opacity-50 bg=transparent rounded-2xl'>Login</Link>
                     <Link href='signup' className='hidden md:block px-4 py-2 text-lg hover:bg-white hover:text-black bg-[#A238FF] rounded-2xl border-2 border-[#A238FF]'>Sign Up</Link>
