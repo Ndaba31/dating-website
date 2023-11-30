945e6c08e36f1d9f96fa13bf84a771bf4c5434ae
burcu apaydin <ndabandaba31@gmail.com>
2023-11-30 03:30:22 +0200

    Cleared everything

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	images: {
-		domains: ['images.unsplash.com'], // Add your desired image host domain here
-	},
-	assetPrefix: process.env.NODE_ENV === 'production' ? '/fonts' : '',
-};
-
-module.exports = nextConfig;
